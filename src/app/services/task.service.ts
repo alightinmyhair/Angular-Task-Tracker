e7340e4d231a55ab78190a5123e07e3e70886f8a
Rachelle Kuhl <alightinmyhair@gmail.com>
2021-10-02 18:03:26 -0500

    Imported JSON Server to serve as a backend/API. App now serves up tasks from fake server

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Task } from '../Task';
 import { TASKS } from '../mock-tasks';
@@ -6,14 +7,15 @@ import { TASKS } from '../mock-tasks';
 @Injectable({
   providedIn: 'root'
 })
+
 export class TaskService {
+  private apiURL = 'http://localhost:5000/tasks';
 
-  constructor() { 
-
+  constructor(private http:HttpClient) {
+     
   }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiURL);
   }
 }
